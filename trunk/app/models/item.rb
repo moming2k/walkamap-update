f7f64133b710a456bf9debf7d610ac29211ebe19
chris <moming2k@gmail.com>
2008-08-30 21:25:43 +0800

    add Item soft deletable

--- trunk/app/models/item.rb
+++ trunk/app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ActiveRecord::Base
   cattr_reader :per_page
   @@per_page = 50
   
+  acts_as_soft_deletable
+  
   def self.find_checked(checked, *args)
     with_scope :find => { :conditions => [ 'is_checked = ?', checked ] } do
       find(:all,*args)
